f7487b3af5d4af9d6e1b6c203af4504a25f9b0a7
haojia <18801407596@163.com>
2018-07-19 10:48:22 +0800

    180719

--- riliben-g/pages/add/add.js
+++ riliben-g/pages/add/add.js
@@ -1,65 +1,132 @@
+//add.js
+const util = require('../../utils/util.js')
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    context: '',
+    mylocation:'',
+    myTags: [],
+    uploadImgs: [],
+  },
+
+  onLoad: function () {
     
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-    
+    //标签
+    this.setData({
+      myTags: wx.getStorageSync('tags') || []
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
+  //textarea输入
+  context: function(e){
+    this.setData({
+      context: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
+  //添加图片
+  uploadImgs: function(){
+    var self = this
+    wx.chooseImage({
+      count: 9, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+
+        self.data.uploadImgs = self.data.uploadImgs.concat(tempFilePaths)
+        self.setData({
+          uploadImgs: self.data.uploadImgs,
+        })
+      }
+    })
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+  //删除图片
+  deleteImg: function(e){
+    var index = e.currentTarget.dataset.index
+    this.data.uploadImgs.splice(index, 1)
     
+    this.setData({
+      uploadImgs: this.data.uploadImgs
+    })
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
+  //查看大图
+  viewImg: function(e){
+    var current = e.currentTarget.dataset.tap
+    var urls = this.data.uploadImgs
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接
+      urls: urls // 需要预览的图片http链接列表
+    })
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  //获取位置
+  mylocation: function(){
+    var self = this;
+    wx.getLocation({
+      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        wx.chooseLocation({
+          success: function(res){
+            self.setData({
+              mylocation: res.name
+            })
+          }
+        })
+      }
+    })
+  },
+  
+  //添加标签
+  addTags: function(){
+    wx.navigateTo({
+      url: '../addTag/addTag'
+    })
+  },
+
+  //发布
+  formSubmit: function(){
+    var context = this.data.context,
+    mylocation = this.data.mylocation,
+    myTags = this.data.myTags,
+    uploadImgs = this.data.uploadImgs
+
+    if (!context && uploadImgs.length==0){
+      wx.showModal({
+        title: '提示',
+        content: '请填写内容',
+      })
+      return false
+    }
+
+    var itemCon = {
+      context: context,
+      mylocation: mylocation,
+      myTags: myTags,
+      uploadImgs: uploadImgs,
+    }
+    var dateId = Date.now()
+    var item = {
+      itemCon: itemCon,
+      dateId: dateId
+    }
+    app.globalData.items.push(item)
     
-  }
+    wx.navigateTo({
+      url: '../index/index'
+    })
+  },
 })
--- riliben-g/pages/addTag/addTag.js
+++ riliben-g/pages/addTag/addTag.js
@@ -1,66 +1,71 @@
-// pages/addTag/addTag.js
+//addTag.js
+const util = require('../../utils/util.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    tagList: [  //标签列表
+      
+    ],
+    tagvalue: '', //input内容
+    ishidden: true,  //确定按钮是否显示
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onLoad: function () {
+    //标签集
+    this.setData({
+      tagList: wx.getStorageSync('tags') || []
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  //点击标签
+  selectTag: function(event){
+    var curTag = 'tagList[' +event.currentTarget.dataset.index+'].isSelected'
+    this.setData({
+      [curTag]: !event.currentTarget.dataset.tab
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  //标签输入
+  inputValue: function(event){
+    this.setData({
+      tagvalue: event.detail.value.trim()
+    })
+    if (this.data.tagvalue) {
+      this.setData({ ishidden: false })
+    }else{
+      this.setData({ ishidden: true })
+    }
+  },
+  //添加标签确认
+  addSubmit: function(){
+    var tagvalue = this.data.tagvalue;
+    this.data.tagList.push({
+      name: tagvalue,
+      isSelected: true
+    })
+    this.setData({
+      tagList: this.data.tagList,
+      tagvalue: '',
+      ishidden: true
+    })
+  },
   
+  //保存标签
+  addTag: function(){
+    var tags = this.data.tagList || []
+    tags.concat(tags)
+    wx.setStorageSync('tags', tags)
+
+    wx.navigateBack()
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  //取消返回上一页
+  goBack: function(){
+    wx.navigateBack()
   }
+  
 })
